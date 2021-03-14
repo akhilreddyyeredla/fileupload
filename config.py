302bccff30302fb655ecbc79df34410efd405168
akhilreddyyeredla <31897102+akhilreddyyeredla@users.noreply.github.com>
2021-03-14 17:38:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-s3_path = 'https://guise.s3.us-east-2.amazonaws.com/'
-bucket_name = 'guise'
+s3_path = 'xxx'
+bucket_name = 'xxxx'
 
 
 #database
-host='gusie.cgmrd6iglvbd.us-east-2.rds.amazonaws.com'
-user='Instance'
-password="#Weneverlose@3532"
-db='guise'
+host='xxxx'
+user='xxxxx'
+password="xxxxx"
+db='xxxxx'
